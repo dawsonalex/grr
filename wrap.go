511b0009679cb4a206983149d5c899ce5ccb1c66
Alex <alexander_dawson@ymail.com>
2026-04-22 11:35:23 +0100

    Have Wrap return Base error type

--- wrap.go
+++ wrap.go
@@ -3,9 +3,9 @@ package errs
 // Wrap attaches a message and stack trace to err, returning a new error that
 // unwraps to err. Use for errors crossing package boundaries that do not
 // warrant a dedicated typed error. Returns nil if err is nil.
-func Wrap(err error, msg string) error {
+func Wrap(err error, msg string) Base {
 	if err == nil {
-		return nil
+		return New(msg)
 	}
 	b := Base{msg: msg}
 	b.ErrStack = CaptureStack(1)
@@ -14,7 +14,7 @@ func Wrap(err error, msg string) error {
 }
 
 // New creates an error with a message and stack trace captured at the call site.
-func New(msg string) error {
+func New(msg string) Base {
 	b := Base{msg: msg}
 	b.ErrStack = CaptureStack(1)
 	return b
